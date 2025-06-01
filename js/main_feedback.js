23f4f80946680976b74eb76775e5dcca9cace8fd
AlexisSTH <sayasith.alexis@gmail.com>
2025-06-01 18:45:41 +0200

    notification, animation
    Test click

---
+++ js/main_feedback.js
@@ -0,0 +1,7 @@
+var notifications = document.getElementsByClassName("notification");
+notifications[0].addEventListener("click", onclicknotification);
+
+// evenement de click sur la notification
+function onclicknotification(){
+    console.log("click");
+}
