cea166b2d22207a1706e336d1a7031f05f0aad52
AlexisSTH <sayasith.alexis@gmail.com>
2025-06-01 20:54:33 +0200

    bouton choix du formulaire

--- js/main_feedback.js
+++ js/main_feedback.js
@@ -1,7 +1,19 @@
 var notifications = document.getElementsByClassName("notification");
-notifications[0].addEventListener("click", onclicknotification);
+notifications[0].addEventListener("mouseenter", onclicknotification);
+
+var content_choices = document.getElementsByClassName("content-choice");
+
+var choice_notifs = document.getElementsByClassName("choice-notif");
+Array.from(choice_notifs).forEach((choise_notif) => choise_notif.addEventListener("click", onclickChoise));
+
 
 // evenement de click sur la notification
 function onclicknotification(){
     console.log("click");
+    content_choices[0].classList.remove("hidden");
+}
+
+// evenement de click sur la notification
+function onclickChoise(){
+    console.log("click choise");
 }
